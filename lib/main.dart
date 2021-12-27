1f845f91dcad72c4423b25a914f88b3713f0b4cd
garysm <gary@sabiasoftware.com>
2021-12-27 11:34:43 -0500

    add showcase text

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-// A Counter example implemented with riverpod
-
 void main() {
   runApp(
-    // Adding ProviderScope enables Riverpod for the entire project
     const ProviderScope(child: MyApp()),
   );
 }
@@ -19,28 +16,17 @@ class MyApp extends StatelessWidget {
   }
 }
 
-/// Providers are declared globally and specifies how to create a state
-final counterProvider = StateProvider((ref) => 0);
-
 class Home extends HookConsumerWidget {
   const Home({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Counter example')),
-      body: Center(
-        // Consumer is a widget that allows you reading providers.
-        // You could also use the hook "useProvider" if you uses flutter_hooks
-        child: Consumer(builder: (context, watch, _) {
-          final count = ref.watch(counterProvider.state).state;
-          return Text('$count');
-        }),
+      appBar: AppBar(
+        title: const Text('Showcase App'),
       ),
-      floatingActionButton: FloatingActionButton(
-        // The read method is an utility to read a provider without listening to it
-        onPressed: () => ref.read(counterProvider.state).state++,
-        child: const Icon(Icons.add),
+      body: const Center(
+        child: Text('Coming soon...'),
       ),
     );
   }
